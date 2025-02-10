b3418162ee5bd9c87af6d741dba9ecf204754ce6
samedkocyigit <abdulsamed.kcygt@gmail.com>
2025-02-11 00:22:42 +0300

    project setup has been arranged

--- UserService/Program.cs
+++ UserService/Program.cs
@@ -1,11 +1,17 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using System.Reflection;
+using System.Text;
 using UserService.Infrastructure.ApplicationDbContext;
+using UserService.Infrastructure.Repositories.UserRepositories;
 using UserService.Services.AuthServices;
 using UserService.Services.EmailServices;
+using UserService.Services.TokenServices;
 using UserService.Services.UserServices;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
 
 builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
 builder.Services.AddHttpClient();
@@ -13,10 +19,29 @@ builder.Services.AddHttpClient();
 builder.Services.AddDbContext<AppDbContext>(options => 
         options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddScoped<IEmailService, EmailService>();
 builder.Services.AddScoped<IUserService, UsersService>();
-builder.Services.AddScoped<IAuthService,AuthService>();
+builder.Services.AddScoped<ITokenService, TokenService>();
+builder.Services.AddScoped<IAuthService, AuthService>();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.RequireHttpsMetadata = false;
+        options.SaveToken = true;
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["JwtSettings:Issuer"],
+            ValidAudience = builder.Configuration["JwtSettings:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JwtSettings:Secret"]))
+        };
+    });
 
 
 builder.Services.AddEndpointsApiExplorer();
@@ -24,12 +49,10 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
 
 app.UseHttpsRedirection();
+app.MapControllers();
 
 app.Run();
