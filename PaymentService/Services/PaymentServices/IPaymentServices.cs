c33146db7be392cbd2ea5daed6ed27877a47a25f
samedkocyigit <abdulsamed.kcygt@gmail.com>
2025-02-11 00:21:20 +0300

    Service files has been defined

---
+++ PaymentService/Services/PaymentServices/IPaymentServices.cs
@@ -0,0 +1,13 @@
+﻿using PaymentService.Domain.Models;
+
+namespace PaymentService.Services.PaymentServices
+{
+    public interface IPaymentServices
+    {
+        Task<List<Payment>> GetAllPayments();
+        Task<Payment> GetPaymentById(Guid id);
+        Task<Payment> CreatePayment(Payment payment);
+        Task<Payment> UpdatePayment(Payment payment);
+        Task DeletePayment(Guid id);
+    }
+}
---
+++ PaymentService/Services/PaymentServices/PaymentsService.cs
@@ -0,0 +1,41 @@
+﻿using PaymentService.Domain.Models;
+using PaymentService.Infrastructure.Repositories.PaymentRepositories;
+
+namespace PaymentService.Services.PaymentServices
+{
+    public class PaymentsService:IPaymentServices
+    {
+        protected readonly IPaymentRepository _paymentRepository;
+        public PaymentsService(IPaymentRepository paymentRepository)
+        {
+            _paymentRepository = paymentRepository;
+        }
+        public async Task<List<Payment>> GetAllPayments()
+        {
+            var payments = await _paymentRepository.GetAllPayments();
+            return payments;
+        }
+        public async Task<Payment> GetPaymentById(Guid id)
+        {
+            var payment = await _paymentRepository.GetPaymentById(id);
+            return payment;
+        }
+        public async Task<Payment> CreatePayment(Payment payment)
+        {
+            var newPayment = await _paymentRepository.CreatePayment(payment);
+            return newPayment;
+        }
+        public async Task<Payment> UpdatePayment(Payment payment)
+        {
+            var updatedPayment = await _paymentRepository.UpdatePayment(payment);
+            return updatedPayment;
+        }
+
+        public async Task DeletePayment(Guid id)
+        {
+            await _paymentRepository.DeletePayment(id);
+        }
+
+        
+    }
+}
