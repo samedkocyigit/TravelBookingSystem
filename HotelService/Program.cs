690f8b5f9e1b127c40c50823ce2ba3ac917752b1
samedkocyigit <abdulsamed.kcygt@gmail.com>
2025-02-11 00:20:03 +0300

    project setup has been arranged

--- HotelService/Program.cs
+++ HotelService/Program.cs
@@ -1,44 +1,39 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.EntityFrameworkCore;
+using HotelService.Infrastructure.ApplicationDbContext;
+using HotelService.Infrastructure.Repositories.FacilityRepositories;
+using HotelService.Infrastructure.Repositories.FloorRepositories;
+using HotelService.Infrastructure.Repositories.HotelRepositories;
+using HotelService.Infrastructure.Repositories.RoomRepositories;
+using HotelService.Services.FacilityServices;
+using HotelService.Services.FloorServices;
+using HotelService.Services.HotelServices;
+using HotelService.Services.RoomServices;
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<AppDbContext>(options =>
+        options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddControllers();
+
+builder.Services.AddScoped<IHotelRepository, HotelRepository>();
+builder.Services.AddScoped<IRoomRepository, RoomRepository>();
+builder.Services.AddScoped<IFloorRepository, FloorRepository>();
+builder.Services.AddScoped<IFacilityRepository, FacilityRepository>();
+
+builder.Services.AddScoped<IRoomService, RoomService>();
+builder.Services.AddScoped<IHotelService,HotelsService>();
+builder.Services.AddScoped<IFloorService, FloorService>();
+builder.Services.AddScoped<IFacilityService, FacilityService>();
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+app.MapControllers();
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
