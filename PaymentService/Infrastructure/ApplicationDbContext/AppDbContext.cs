fe846205f57baf16158b7f9288099a6ee7fdb9b8
samedkocyigit <abdulsamed.kcygt@gmail.com>
2025-02-11 00:20:48 +0300

    ApplicationDbContext Has been created

---
+++ PaymentService/Infrastructure/ApplicationDbContext/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using PaymentService.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookingService.Infrastructure.ApplicationDbContext
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions options) :base(options)
+        {
+            
+        }
+
+        public DbSet<Payment> Payments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
