11212aa2532251a763d8eafbf867074545432449
samedkocyigit <abdulsamed.kcygt@gmail.com>
2025-02-11 00:16:01 +0300

    model folder has been created

---
+++ FlightService/Domain/Models/Flight.cs
@@ -0,0 +1,12 @@
+﻿namespace FlightService.Domain.Models
+{
+    public class Flight
+    {
+        public Guid Id { get; set; }
+        public DateTime FlightTime { get; set; }
+        public string CompanyName { get; set; }
+        public decimal Price { get; set; }
+        
+
+    }
+}
