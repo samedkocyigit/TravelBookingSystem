57c619f3101e65e287925b157c8a1ab61013d480
samedkocyigit <abdulsamed.kcygt@gmail.com>
2025-02-11 00:14:59 +0300

    Service files has been implemented

---
+++ BookingService/Services/BookingServices/BookingsService.cs
@@ -0,0 +1,41 @@
+﻿using BookingService.Domain.Models;
+using BookingService.Infrastructure.Repositories.BookingRepositories;
+
+namespace BookingService.Services.BookingServices
+{
+    public class BookingsService:IBookingServices
+    {
+        protected readonly IBookingRepository _bookingRepository;
+        public BookingsService(IBookingRepository bookingRepository)
+        {
+            _bookingRepository = bookingRepository;
+        }
+        public async Task<List<Booking>> GetAllBookings()
+        {
+            var bookings = await _bookingRepository.GetAllBookings();
+            return bookings;
+        }
+        public async Task<Booking> GetBookingById(Guid id)
+        {
+            var booking = await _bookingRepository.GetBookingById(id);
+            return booking;
+        }
+        public async Task<Booking> CreateBooking(Booking booking)
+        {
+            var newBooking = await _bookingRepository.CreateBooking(booking);
+            return newBooking;
+        }
+        public async Task<Booking> UpdateBooking(Booking booking)
+        {
+            var updatedBooking = await _bookingRepository.UpdateBooking(booking);
+            return updatedBooking;
+        }
+
+        public async Task DeleteBooking(Guid id)
+        {
+            await _bookingRepository.DeleteBooking(id);
+        }
+
+        
+    }
+}
---
+++ BookingService/Services/BookingServices/IBookingServices.cs
@@ -0,0 +1,13 @@
+﻿using BookingService.Domain.Models;
+
+namespace BookingService.Services.BookingServices
+{
+    public interface IBookingServices
+    {
+        Task<List<Booking>> GetAllBookings();
+        Task<Booking> GetBookingById(Guid id);
+        Task<Booking> CreateBooking(Booking booking);
+        Task<Booking> UpdateBooking(Booking booking);
+        Task DeleteBooking(Guid id);
+    }
+}
