74851e5d19f6fc30db7119e2e3c6e24b6c2ab566
samedkocyigit <abdulsamed.kcygt@gmail.com>
2025-02-11 00:16:58 +0300

    Service files has been implemented

---
+++ FlightService/Services/FlightServices/FlightsService.cs
@@ -0,0 +1,41 @@
+﻿using FlightService.Domain.Models;
+using FlightService.Infrastructure.Repositories.FlightRepository;
+
+namespace FlightService.Services.FlightServices
+{
+    public class FlightsService: IFlightService
+    {
+        protected readonly IFlightRepository _flightRepository;
+        public FlightsService(IFlightRepository flightRepository)
+        {
+            _flightRepository = flightRepository;
+        }
+
+        public async Task<List<Flight>> GetAllFlights()
+        {
+            var flights = await _flightRepository.GetAllFlights();
+            return flights;
+        }
+        public async Task<Flight> GetFlightById(Guid id)
+        {
+            var flight = await _flightRepository.GetFlightById(id);
+            return flight;
+        }
+        public async Task<Flight> CreateFlight(Flight flight)
+        {
+            var newFlight = await _flightRepository.CreateFlight(flight);
+            return newFlight;
+        }
+
+        public async Task<Flight> UpdateFlight(Flight flight)
+        {
+            var updatedFlight = await _flightRepository.UpdateFlight(flight);
+            return updatedFlight;
+        }
+
+        public async Task DeleteFlight(Guid id)
+        {
+            await _flightRepository.GetFlightById(id);
+        }
+    }
+}
---
+++ FlightService/Services/FlightServices/IFlightService.cs
@@ -0,0 +1,13 @@
+﻿using FlightService.Domain.Models;
+
+namespace FlightService.Services.FlightServices
+{
+    public interface IFlightService
+    {
+        Task<List<Flight>> GetAllFlights();
+        Task<Flight> GetFlightById(Guid id);
+        Task<Flight> CreateFlight(Flight flight);
+        Task<Flight> UpdateFlight(Flight flight);
+        Task DeleteFlight(Guid id);
+    }
+}
