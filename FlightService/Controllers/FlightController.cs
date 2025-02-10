a575f819e0da531c0af055cbf5998953ca6b7899
samedkocyigit <abdulsamed.kcygt@gmail.com>
2025-02-11 00:15:43 +0300

    Controller has been created

---
+++ FlightService/Controllers/FlightController.cs
@@ -0,0 +1,50 @@
+﻿using FlightService.Domain.Models;
+using FlightService.Services.FlightServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlightService.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class FlightController:ControllerBase
+    {
+        private readonly IFlightService _flightService;
+        public FlightController(IFlightService flightService)
+        {
+            _flightService = flightService;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAllFlights()
+        {
+            var flights = await _flightService.GetAllFlights();
+            return Ok(flights);
+        }
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<IActionResult> GetFlightById(Guid id)
+        {
+            var flight = await _flightService.GetFlightById(id);
+            return Ok(flight);
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateFlight(Flight flight)
+        {
+            var newFlight = await _flightService.CreateFlight(flight);
+            return Ok(newFlight);
+        }
+        [HttpPut]
+        public async Task<IActionResult> UpdateFlight(Flight flight)
+        {
+            var updatedFlight = await _flightService.UpdateFlight(flight);
+            return Ok(updatedFlight);
+        }
+        [HttpDelete]
+        [Route("{id}")]
+        public async Task<IActionResult> DeleteFlight(Guid id)
+        {
+            await _flightService.DeleteFlight(id);
+            return Ok();
+        }
+
+    }
+}
