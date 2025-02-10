b4603e8ad6ea1365d66314e0ad7857484a8b6764
samedkocyigit <abdulsamed.kcygt@gmail.com>
2025-02-11 00:20:31 +0300

    Enum and Models has been created

---
+++ PaymentService/Domain/Enums/PaymentStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace PaymentService.Domain.Enums
+{
+    public enum PaymentStatus
+    {
+        Pending,
+        Completed,
+        Failed
+    }
+}
---
+++ PaymentService/Domain/Models/Payment.cs
@@ -0,0 +1,14 @@
+﻿using PaymentService.Domain.Enums;
+
+namespace PaymentService.Domain.Models
+{
+    public class Payment
+    {
+        public Guid Id { get; set; }
+        public Guid BookingId { get; set; }  
+        public decimal Amount { get; set; }
+        public string PaymentMethod { get; set; } = "CreditCard";
+        public PaymentStatus Status { get; set; } = PaymentStatus.Pending;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
