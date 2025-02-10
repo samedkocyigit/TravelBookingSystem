372c234d5fe245be5df622349b38f6115ad576ca
samedkocyigit <abdulsamed.kcygt@gmail.com>
2025-02-11 00:14:13 +0300

    ApplicationDbContext has been created

---
+++ BookingService/Infrastructure/ApplicationDbContext/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using BookingService.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookingService.Infrastructure.ApplicationDbContext
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions options) :base(options)
+        {
+            
+        }
+
+        public DbSet<Booking> Bookings { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
