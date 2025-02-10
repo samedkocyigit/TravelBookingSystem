7fe979ff5a0e945f0ad7695db31cc055e07c917c
samedkocyigit <abdulsamed.kcygt@gmail.com>
2025-02-11 00:16:13 +0300

    ApplicationDbContext has been created

---
+++ FlightService/Infrastructure/ApplicationDbContext/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using FlightService.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FlightService.Infrastructure.ApplicationDbContext
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions options) :base(options)
+        {
+            
+        }
+
+        public DbSet<Flight> Flights { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
