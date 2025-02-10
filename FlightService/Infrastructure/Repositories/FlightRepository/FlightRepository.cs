7398db8c394b1a1ba3f44114a57c0c09c1377014
samedkocyigit <abdulsamed.kcygt@gmail.com>
2025-02-11 00:16:30 +0300

    Repositories has been implemented

---
+++ FlightService/Infrastructure/Repositories/FlightRepository/FlightRepository.cs
@@ -0,0 +1,43 @@
+﻿using FlightService.Domain.Models;
+using FlightService.Infrastructure.ApplicationDbContext;
+using Microsoft.EntityFrameworkCore;
+
+namespace FlightService.Infrastructure.Repositories.FlightRepository
+{
+    public class FlightRepository:IFlightRepository
+    {
+        protected readonly AppDbContext _context;
+        public FlightRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Flight>> GetAllFlights()
+        {
+            return await _context.Flights.ToListAsync();
+        }
+        public async Task<Flight> GetFlightById(Guid id)
+        {
+            return await _context.Flights.FirstOrDefaultAsync(u => u.Id == id);
+        }
+
+        public async Task<Flight> CreateFlight(Flight flight)
+        {
+            _context.Flights.Add(flight);
+            await _context.SaveChangesAsync();
+            return flight;
+        }
+        public async Task<Flight> UpdateFlight(Flight flight)
+        {
+            _context.Flights.Update(flight);
+            await _context.SaveChangesAsync();
+            return flight;
+        }
+        public async Task DeleteFlight(Guid id)
+        {
+            var flight = await GetFlightById(id);
+            _context.Flights.Remove(flight);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ FlightService/Infrastructure/Repositories/FlightRepository/IFlightRepository.cs
@@ -0,0 +1,13 @@
+﻿using FlightService.Domain.Models;
+
+namespace FlightService.Infrastructure.Repositories.FlightRepository
+{
+    public interface IFlightRepository
+    {
+        Task<List<Flight>> GetAllFlights();
+        Task<Flight> GetFlightById(Guid id);
+        Task<Flight> CreateFlight(Flight flight);
+        Task<Flight> UpdateFlight(Flight flight);
+        Task DeleteFlight(Guid id);
+    }
+}
