38ea970f04ace3d3b092a27195d417362897c4ad
samedkocyigit <abdulsamed.kcygt@gmail.com>
2025-02-11 00:16:40 +0300

    migrations has been added

---
+++ FlightService/Migrations/20250210201244_createInitial.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FlightService.Migrations
+{
+    /// <inheritdoc />
+    public partial class createInitial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Flights",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    FlightTime = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    CompanyName = table.Column<string>(type: "text", nullable: false),
+                    Price = table.Column<decimal>(type: "numeric", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Flights", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Flights");
+        }
+    }
+}
