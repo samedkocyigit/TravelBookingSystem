857c8f31af9fd2d5555fb8188688e1a08be1784e
samedkocyigit <abdulsamed.kcygt@gmail.com>
2025-02-13 14:44:39 +0300

    local changes has been arranged

--- FlightService/Program.cs
+++ FlightService/Program.cs
@@ -1,5 +1,15 @@
 using FlightService.Infrastructure.ApplicationDbContext;
-using FlightService.Infrastructure.Repositories.FlightRepository;
+using FlightService.Infrastructure.Repositories.AircraftRepositories;
+using FlightService.Infrastructure.Repositories.AirportRepositories;
+using FlightService.Infrastructure.Repositories.BaggageAllowanceRepositories;
+using FlightService.Infrastructure.Repositories.FlightCompanyCompanyRepositories;
+using FlightService.Infrastructure.Repositories.FlightRepositories;
+using FlightService.Infrastructure.Repositories.SeatRepositories;
+using FlightService.Infrastructure.Repositories.TicketPriceRepositories;
+using FlightService.Services.AircraftServices;
+using FlightService.Services.AirportServices;
+using FlightService.Services.BaggageAllowanceServices;
+using FlightService.Services.FlightCompanyServices;
 using FlightService.Services.FlightServices;
 using Microsoft.EntityFrameworkCore;
 
@@ -13,10 +23,18 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 builder.Services.AddControllers();
 
 builder.Services.AddScoped<IFlightRepository, FlightRepository>();
+builder.Services.AddScoped<IFlightCompanyRepository,FlightCompanyRepository>();
+builder.Services.AddScoped<ISeatRepository,SeatRepository>();
+builder.Services.AddScoped<IAircraftRepository,AircraftRepository>();
+builder.Services.AddScoped<IAirportRepository,AirportRepository>();
+builder.Services.AddScoped<IBaggageAllowanceRepository,BaggageAllowanceRepository>();
+builder.Services.AddScoped<ITicketPriceRepository,TicketPriceRepository>();
+
 
 builder.Services.AddScoped<IFlightService, FlightsService>();
 
 var app = builder.Build();
+MigrationService.InitializeMigration(app);
 
 app.UseSwagger();
 app.UseSwaggerUI();
