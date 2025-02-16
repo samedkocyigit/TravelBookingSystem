c91ee18e34a4264b4ba43b080f44293275e85c45
samedkocyigit <abdulsamed.kcygt@gmail.com>
2025-02-16 13:15:40 +0300

    repositories has been seperated hotel and flight

---
+++ BookingService/Infrastructure/Repositories/FlightBookingRepositories/IFlightBookingRepository.cs
@@ -0,0 +1,13 @@
+﻿using BookingService.Domain.Models;
+
+namespace BookingService.Infrastructure.Repositories.FlightBookingRepositories
+{
+    public interface IFlightBookingRepository
+    {
+        Task<List<FlightBooking>> GetAllBookings();
+        Task<FlightBooking> GetBookingById(Guid id);
+        Task<FlightBooking> CreateBooking(FlightBooking booking);
+        Task<FlightBooking> UpdateBooking(FlightBooking booking);
+        Task DeleteBooking(Guid id);
+    }
+}
---
+++ BookingService/Infrastructure/Repositories/HotelBookingRepositories/IHotelBookingRepository.cs
@@ -0,0 +1,13 @@
+﻿using BookingService.Domain.Models;
+
+namespace BookingService.Infrastructure.Repositories.HotelBookingRepositories
+{
+    public interface IHotelBookingRepository
+    {
+       Task<List<HotelBooking>> GetAllBookings();
+        Task<HotelBooking> GetBookingById(Guid id);
+        Task<HotelBooking> CreateBooking(HotelBooking booking);
+        Task<HotelBooking> UpdateBooking(HotelBooking booking);   
+        Task DeleteBooking(Guid id);
+    }
+}
