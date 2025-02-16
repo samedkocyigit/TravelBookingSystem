a1eba91c34192cf39515968c81db8f07b6eb553e
samedkocyigit <abdulsamed.kcygt@gmail.com>
2025-02-16 23:04:56 +0300

    createUserDto has been created

---
+++ UserService/Domain/Dtos/CreateUserDto.cs
@@ -0,0 +1,16 @@
+﻿using UserService.Domain.Enums;
+
+namespace UserService.Domain.Dtos
+{
+    public class CreateUserDto
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string? Name { get; set; }
+        public string? Surname { get; set; }
+        public int? Age { get; set; }
+        public string? Nationality { get; set; }
+        public Roles? Roles { get; set; }
+    }
+}
