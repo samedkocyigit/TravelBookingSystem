6aa5b991ada88dc2ab5ed9c84618b42c2a18095c
samedkocyigit <abdulsamed.kcygt@gmail.com>
2025-02-16 13:12:00 +0300

    new dtos has been created for availableRooms

---
+++ HotelService/Domain/Dtos/AvailableRoomDto.cs
@@ -0,0 +1,15 @@
+﻿using HotelService.Domain.Enums;
+using HotelService.Models.Enums;
+
+namespace HotelService.Domain.Dtos
+{
+    public class AvailableRoomDto
+    {
+        public Guid Id { get; set; } 
+        public RoomType RoomType { get; set; }
+        public int RoomCapacity { get; set; }
+        public decimal PricePerNight { get; set; }
+        public int RoomNumber { get; set; }
+        public IsBooked IsBooked { get; set; } 
+    }
+}
