bd709c501db5a2c3186a3674eaeac09c46ae7f22
samedkocyigit <abdulsamed.kcygt@gmail.com>
2025-02-16 15:54:34 +0300

    Hotel and room dtos has been created for hotelService response

---
+++ BookingService/Domain/Dtos/RoomDto.cs
@@ -0,0 +1,12 @@
+﻿namespace BookingService.Domain.Dtos
+{
+    public class RoomDto
+    {
+        public Guid Id { get; set; }
+        public string RoomType { get; set; }
+        public int RoomCapacity { get; set; }
+        public decimal PricePerNight { get; set; }
+        public int RoomNumber { get; set; }
+        public string IsBooked { get; set; }
+    }
+}
