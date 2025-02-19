b6e368d5d7c677edeab8d4b7b8e6fa6a46984a3b
sam <abdulsamed.kcygt@gmail.com>
2025-02-19 20:23:19 +0300

    dtos has been refactored

--- BookingService/Domain/Dtos/RoomDto.cs
+++ BookingService/Domain/Dtos/RoomDto.cs
@@ -3,10 +3,10 @@
     public class RoomDto
     {
         public Guid Id { get; set; }
-        public string RoomType { get; set; }
+        public int RoomType { get; set; }
         public int RoomCapacity { get; set; }
         public decimal PricePerNight { get; set; }
         public int RoomNumber { get; set; }
-        public string IsBooked { get; set; }
+        public int IsBooked { get; set; }
     }
 }
