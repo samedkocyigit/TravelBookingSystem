94b5d43a28e66a57dbddb688421a68c86efa2c50
sam <abdulsamed.kcygt@gmail.com>
2025-02-21 16:51:46 +0300

    Payment Model has been created

---
+++ UserService/Domain/Models/Payment.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json.Serialization;
+
+namespace UserService.Domain.Models
+{
+    public class Payment
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string CardNumber { get; set; }  
+        public string CardHolderName { get; set; }
+        public string ExpiryDate { get; set; }
+        public string CVV { get; set; }
+        public decimal PaymentLimit { get; set; } = 10000;
+        public Guid UserId { get; set; }
+        [JsonIgnore]    
+        public User? User { get; set; }
+    }
+}
