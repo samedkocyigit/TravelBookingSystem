518fac8d25ba6c5757f4ff176b1e6f0c033c0f32
sam <abdulsamed.kcygt@gmail.com>
2025-02-22 14:41:59 +0300

    migrations Added

--- PaymentService/Migrations/20250221203922_InitialCreate.Designer.cs
+++ PaymentService/Migrations/20250221203922_InitialCreate.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using BookingService.Infrastructure.ApplicationDbContext;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace PaymentService.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250221203922_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
