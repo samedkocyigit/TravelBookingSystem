946f0141e80a30ca731f325d3d4c4b88cc4c34a2
sam <abdulsamed.kcygt@gmail.com>
2025-02-22 14:41:11 +0300

    Payment Model Has been refactored

--- PaymentService/Domain/Models/Payment.cs
+++ PaymentService/Domain/Models/Payment.cs
@@ -4,11 +4,14 @@ namespace PaymentService.Domain.Models
 {
     public class Payment
     {
-        public Guid Id { get; set; }
-        public Guid BookingId { get; set; }  
-        public decimal Amount { get; set; }
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public Guid UserToBeCharged { get; set; }
+        public Guid BookingId { get; set; }
+        public string CardNumber { get; set; }
+        public string CardHolderName { get; set; }
+        public string ExpiryDate { get; set; }
+        public string CVV { get; set; }
         public string PaymentMethod { get; set; } = "CreditCard";
-        public PaymentStatus Status { get; set; } = PaymentStatus.Pending;
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     }
 }
