8fe24bad3eb6637cbebe9596f1e6d67830996241
sam <abdulsamed.kcygt@gmail.com>
2025-02-22 14:42:22 +0300

    new mapper func has been created

---
+++ PaymentService/Services/Profiles/AutoMapperProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using PaymentService.Domain.Dtos;
+using PaymentService.Domain.Models;
+
+namespace PaymentService.Services.Profiles
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<PaymentCreationDto, Payment>()
+                .ForMember(dest => dest.CardNumber, opt => opt.MapFrom(src => src.CardNumber))
+                .ForMember(dest => dest.CardHolderName, opt => opt.MapFrom(src => src.CardHolderName))
+                .ForMember(dest => dest.ExpiryDate, opt => opt.MapFrom(src => src.ExpiryDate))
+                .ForMember(dest => dest.CVV, opt => opt.MapFrom(src => src.CVV))
+                .ForMember(dest => dest.UserToBeCharged, opt => opt.MapFrom(src => src.UserToBeCharged))
+                .ForMember(dest => dest.BookingId, opt => opt.MapFrom(src => src.BookingId))
+                .ReverseMap();
+        }
+    }
+}
