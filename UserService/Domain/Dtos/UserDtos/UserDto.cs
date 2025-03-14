d699875cda6340f6e83140e411c3b992ae423d84
sam <abdulsamed.kcygt@gmail.com>
2025-03-14 17:16:25 +0300

    some properties has been becomed nullable

--- UserService/Domain/Dtos/UserDtos/UserDto.cs
+++ UserService/Domain/Dtos/UserDtos/UserDto.cs
@@ -5,16 +5,16 @@ namespace UserService.Domain.Dtos.UserDtos
     public class UserDto
     {
         public Guid id { get; set; }
-        public string name { get; set; }
-        public string surname { get; set; }
+        public string? name { get; set; }
+        public string? surname { get; set; }
         public string email { get; set; }
         public string username { get; set; }
-        public int age { get; set; }
-        public string nationality { get; set; }
-        public DateTime birthday { get; set; }
-        public List<Payment> payments { get; set; }
-        public List<Guid> flightBookingIds { get; set; }
-        public List<Guid> hotelBookingIds { get; set; }
+        public int? age { get; set; }
+        public string? nationality { get; set; }
+        public DateTime? birthday { get; set; }
+        public List<Payment>? payments { get; set; }
+        public List<Guid>? flightBookingIds { get; set; }
+        public List<Guid>? hotelBookingIds { get; set; }
 
     }
 }
