e6e6c7a876478e70d63e4a9a88165baa245fbd1a
sam <abdulsamed.kcygt@gmail.com>
2025-03-14 17:12:18 +0300

    Authentication and role based authorization has been implemented to endpoints

--- UserService/Controllers/PaymentController.cs
+++ UserService/Controllers/PaymentController.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using UserService.Domain.Dtos.PaymentDtos;
 using UserService.Domain.Models;
 using UserService.Services.PaymentServices;
 
 namespace UserService.Controllers
 {
+    [Authorize]
     [ApiController]
     [Route("api/[controller]")]
     public class PaymentController : ControllerBase
@@ -14,18 +16,22 @@ namespace UserService.Controllers
         {
             _paymentService = paymentService;
         }
+
+        [Authorize(Roles = "Admin,Manager")]
         [HttpGet]
         public async Task<IActionResult> GetAllPayments()
         {
             var payments = await _paymentService.GetAllPayments();
             return Ok(payments);
         }
+
         [HttpPost]
         public async Task<IActionResult> CreatePayment(PaymentCreateDto payment)
         {
             var newPayment = await _paymentService.CreatePayment(payment);
             return Ok(newPayment);
         }
+
         [HttpGet]
         [Route("{id}")]
         public async Task<IActionResult> GetPaymentById(Guid id)
@@ -33,12 +39,14 @@ namespace UserService.Controllers
             var payment = await _paymentService.GetPaymentById(id);
             return Ok(payment);
         }
+
         [HttpPut]
         public async Task<IActionResult> UpdatePayment(Payment payment)
         {
             var updatedPayment = await _paymentService.UpdatePayment(payment);
             return Ok(updatedPayment);
         }
+
         [HttpDelete]
         [Route("{id}")]
         public async Task<IActionResult> DeletePayment(Guid id)
