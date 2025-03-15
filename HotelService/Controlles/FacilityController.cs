8f703510b319b599be9f2fd96a60da495a4cd193
sam <abdulsamed.kcygt@gmail.com>
2025-03-15 13:59:31 +0300

    necessary endpoints has been authorized

--- HotelService/Controlles/FacilityController.cs
+++ HotelService/Controlles/FacilityController.cs
@@ -1,6 +1,7 @@
 ﻿using HotelService.Domain.Dtos;
 using HotelService.Models.Models;
 using HotelService.Services.FacilityServices;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HotelService.Controlles
@@ -14,12 +15,16 @@ namespace HotelService.Controlles
         {
             _facilityService = facilityService;
         }
+
+        //get all facilities
         [HttpGet]
         public async Task<IActionResult> GetAllFacilitys()
         {
             var facilities = await _facilityService.GetAllFacilities();
             return Ok(facilities);
         }
+
+        //get facility by id
         [HttpGet]
         [Route("{id}")]
         public async Task<IActionResult> GetFacilityById(Guid id)
@@ -27,18 +32,27 @@ namespace HotelService.Controlles
             var facility = await _facilityService.GetFacilityById(id);
             return Ok(facility);
         }
+
+        //creation facility
+        [Authorize(Roles = "Admin,Manager")]
         [HttpPost]
         public async Task<IActionResult> CreateFacility(FacilityCreationDto facilityDto)
         {
             var newFacility = await _facilityService.CreateFacility(facilityDto);
             return Ok(newFacility);
         }
+
+        //update facility
+        [Authorize(Roles = "Admin,Manager")]
         [HttpPut]
         public async Task<IActionResult> UpdateFacility(Facility facility)
         {
             var updatedFacility = await _facilityService.UpdateFacility(facility);
             return Ok(updatedFacility);
         }
+
+        //delete facility with id
+        [Authorize(Roles = "Admin")]
         [HttpDelete]
         [Route("{id}")]
         public async Task<IActionResult> DeleteFacility(Guid id)
